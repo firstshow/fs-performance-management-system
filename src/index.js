2ad39d33347329fa3797b2651b70deae6ce4450d
zhangcheng <776331229@qq.com>
2018-07-13 00:04:36 +0800

    完成登陆页面

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-import App from '~pages/App.js'
+import App from '~pages/App'
 import { Provider } from 'react-redux'
 import store from './store/index.js'
 import registerServiceWorker from './registerServiceWorker'
+
+import 'flex.css/dist/data-flex.css';
 
 ReactDOM.render(
     <Provider store={store}>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -8,33 +8,83 @@ class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: store.getState().Login.username,
+            username: '', // 用户名
+            password: '', // 密码
+            btnState: true // 按钮状态，当没有输入用户名或者密码的时候，按钮置灰
         }
     }
 
+    /**
+     * 当页面第一次渲染完成的生命周期函数
+     */
     componentDidMount() {
         console.log('生成成功')
-        // initFun()
-        // canvas.render()
+    }
+
+    /**
+     * 当用户名输入框变化
+     * @param event 输入框对象
+     */
+    handelUserChange(event) {
+        this.setState({
+            username: event.target.value
+        }, () => {
+            this.checkBtnState()
+        })
+
+    }
+
+    /**
+     * 当密码输入框变化
+     * @param event 输入框对象
+     */
+    handelPassChange(event) {
+        this.setState({
+            password: event.target.value
+        }, () => {
+            this.checkBtnState()
+        })
+    }
+
+    /**
+     * 当用户名或者密码为空的时候，按钮不可点击
+     *
+     */
+    checkBtnState() {
+        if (!this.state.username || !this.state.password) {
+            this.setState({
+                btnState: true
+            })
+        } else {
+            this.setState({
+                btnState: false
+            })
+        }
     }
 
     /**
      * 登陆
      */
     handleLogin() {
-        store.dispatch(loginActions.login({
-            username: 111
-        }))
-        this.setState({
-            username: store.getState().Login.username,
-        })
+        // store.dispatch(loginActions.login({
+        //     username: 111
+        // }))
+       alert('登陆成功')
     }
 
     render() {
         return (
-            <div className="App">
-                <Button type="primary" onClick={this.handleLogin.bind(this)}>登陆{this.state.username}</Button>
-                <p>我是登陆页面</p>
+            <div className="x-login-container" data-flex="dir:top main:center cross:center">
+                <h1>噼里啪啦团队</h1>
+                <div className="x-login-content" data-flex="dir:top">
+                    <label htmlFor="username">
+                        <input id="username" ref="username" type="text" placeholder="请输入用户名" onChange={this.handelUserChange.bind(this)}/>
+                    </label>
+                    <label htmlFor="password">
+                        <input id="password" ref="password" type="password" placeholder="请输入密码" onChange={this.handelPassChange.bind(this)}/>
+                    </label>
+                </div>
+                <Button className="x-login-btn" type="primary" disabled={this.state.btnState} onClick={this.handleLogin.bind(this)}>登陆</Button>
             </div>
         )
     }
